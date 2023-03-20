b7a235690f9c213e284152d2f1c41ba3f709b8f5
Molly Shillabeer <mollyshillabeer@gmail.com>
2023-03-20 10:45:59 -0400

    fitness calculation fixed

--- evaluation.py
+++ evaluation.py
@@ -1,10 +1,13 @@
-def fitness(individual): 
+def fitness(individual,rooms,guests): 
     """Compute fitness of an invidual solution (maximization)"""    
 
     fitness = 0
-    for room in individual:
-        if room != -1:
-            #TODO: added cost per person, ocean view, etc
-            fitness+=room.cost
-    
+    for i in range(len(individual)):
+        currRoom=rooms[i]
+        for j in range(len(individual[i])):
+            if individual[i][j] != -1:
+                #TODO: accessibility, ocean view, etc
+                #(cost+cost per person*num people)*duration
+                currGuest=guests[individual[i][j]]
+                fitness+=(currRoom.getCost()+currRoom.getPerPersonCost()*currGuest.getSize())*currGuest.getNumDays()
     return fitness
