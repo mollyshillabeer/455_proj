af9ca35291fe2f24daa500e1e4d6993dd3b31874
azingman <72311173+azingman@users.noreply.github.com>
2023-03-29 19:05:48 -0400

    Update mutation.py
    Final mutation function

--- mutation.py
+++ mutation.py
@@ -3,17 +3,21 @@ from Guest import Guest
 from Hotel import HotelRoom
 
 def permutation_swap (individual,rooms,guests):
+    
     match = False
     mutant = individual.copy()
-    while match != True:
+    tries = 0
+
+    while match != True and tries != 10:
         randomRoom = random.choice(rooms)
         randomGuest = random.choice(guests) 
         guestIndex = guests.index(randomGuest)
         roomIndex = rooms.index(randomRoom) 
         if guests[guestIndex].size <= rooms[roomIndex].size:
-            randomGuestToSwitch = random.randint(0,1)
+            randomGuestToSwitch = random.randint(0,len(individual)-1)
             mutant[roomIndex][randomGuestToSwitch] = guestIndex
             match = True     
         else:
+            tries +=1
             match = False
     return mutant
